c3a12fbe3bcabb7eb2b76be1570381f985f0d998
YunZhou Liu <lyz9928@p0056.ten.osc.edu>
2022-04-05 16:21:30 -0400

    one more function

---
+++ R/Hoyd_list.R
@@ -0,0 +1,26 @@
+#'
+#'
+#' @param ptable.df output from function survivalByQuantile. It includes information about
+#' survival under various threshold
+#' @param taxlev a character object specifying which subject survival analysis
+#' is applying to.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+make_threshold <- function(ptable.df,taxlev){
+  tmp <- ggplot(tresh[[taxlev]], aes(x=percentile, y=log(pval)))+
+    geom_line(aes(color = hazard.direction, group=1))+
+    geom_hline(yintercept = log(0.05))+
+    ylab("log(pval)")+
+    scale_color_manual(breaks = c(">=1", "<1"), values = c("red", "black")) +
+    theme_bw()+
+    ggtitle(taxlev)
+    # theme(plot.title = element_text(size = 35),
+    #       legend.text = element_text(size= 30),
+    #       legend.title = element_text(size= 30),
+    #       axis.text=element_text(size=25),
+    #       axis.title=element_text(size=25))
+  return(tmp)
+}
