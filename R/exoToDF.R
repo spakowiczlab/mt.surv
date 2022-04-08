31bc837de1143c609262e28681609257e418c1e4
YunZhou Liu <lyz9928@p0091.ten.osc.edu>
2022-04-08 14:21:26 -0400

    update exoToDF function

---
+++ R/exoToDF.R
@@ -0,0 +1,33 @@
+#' change data frame into wide format
+#'
+#' @param data input data in long format
+#' @param surv survival information for the data
+#' @param taxalevels character vector
+#'
+#' @return
+#' @export
+#'
+#' @examples
+exoToDF <- function(data,surv,
+                    taxalevels = c("domain", "kingdom", "phylum", "class",
+                                   "order", "family", "genus", "species")){
+  exoRAtowide <- function(data,surv, taxlev){
+    tmp <- data %>%
+      select(ID,exo.ra)
+    tmp$Taxa <- data[[taxlev]]
+    tmp.wide <- tmp %>%
+      group_by(ID,Taxa)%>%
+      summarize(ra = sum(exo.ra, na.rm = T))%>%
+      ungroup()%>%
+      spread(key = "Taxa", value = "ra")%>%
+      filter(ID %in% surv$ID)
+    tmp.wide[is.na(tmp.wide)] <- 0
+    return(tmp.wide)
+  }
+  tmp.wide[is.na(tmp.wide)] <- 0
+  w.ls <- lapply(taxalevels, function(x) exoRAtowide(data,surv, x))
+  names(w.ls) <- taxalevels
+
+  #w.df <- purrr::reduce(w.ls, function(x,y) left_join(x,y))
+  return(w.ls)
+}
