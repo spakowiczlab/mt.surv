1eee8714c66110d7edcfd9ea3e9d26fe9388e4d4
YunZhou Liu <lyz9928@p0003.ten.osc.edu>
2022-06-06 13:11:02 -0400

    update documentation

--- R/survival_plot.R
+++ R/survival_plot.R
@@ -18,11 +18,11 @@ survival_plot <- function(modified_input, taxlev, title.input){
   surv$Taxa <- tax.mat[[taxlev]]
 
   tre <- tresh[[taxlev]] %>%
-    arrange(pval) %>%
-    filter(!pval<pval[1]) %>%
-    select(percentile)
+    dplyr::arrange(pval) %>%
+    dplyr::filter(!pval<pval[1]) %>%
+    dplyr::select(percentile)
 
-  cut <- quantile(surv$Taxa, tre[1,])
+  cut <- stats::quantile(surv$Taxa, tre[1,])
 
   tmp <- gsub("[a-z]__(.*)","\\1",taxlev)
 
@@ -36,16 +36,6 @@ survival_plot <- function(modified_input, taxlev, title.input){
                        pval = TRUE,
                        title = paste(tmp," (",title.input,")",sep = ""),
                        pval.coord = c(3500, 0.9))
-
-  # p.temp$plot <- p.temp$plot +
-  #   theme(plot.title = element_text(size=10))
-  # theme(axis.text.y = element_blank(),
-  #       axis.text.x = element_blank(),
-  #       panel.background = element_rect(fill='transparent'),
-  #       plot.background = element_rect(fill='transparent', color=NA),
-  #       panel.grid.major = element_blank(),
-  #       panel.grid.minor = element_blank())+
-  # ggtitle(paste(taxlev," (",modified_input,")",sep = ""))
 
   return(p.temp)
 }
